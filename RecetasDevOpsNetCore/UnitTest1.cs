80a8035f5e80532e43ef0c37356b9953c210b086
alex <alejandro.garcia-miravet@sogeti.com>
2018-11-04 10:12:22 +0100

    +First commit

---
+++ RecetasDevOpsNetCore/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+
+namespace RecetasDevOpsNetCore
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(4, 4);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal(4, 4);
+        }
+
+    }
+}
